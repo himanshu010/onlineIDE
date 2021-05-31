2b08a393943ba4bf7feba5bcf4dd7e1a66c4024c
himanshu010 <aswalhimanshu853@gmail.com>
2021-05-31 10:58:59 +0530

    oath

--- src/utils/getRepo.js
+++ src/utils/getRepo.js
@@ -1,33 +1,66 @@
 var request = require("postman-request");
 
-async function getRepo(username, repo, structure) {
-  const options = {
-    url:
-      "https://api.github.com/repos/" +
-      username +
-      "/" +
-      repo +
-      "/contents" +
-      "/" +
-      structure,
-    method: "GET",
-    headers: {
-      "User-Agent": "request",
-    },
-  };
+async function getRepo(username, repo, structure, token) {
+  if (token) {
+    console.log("with_token");
+    var b_token = "Bearer " + token;
+    const options = {
+      url:
+        "https://api.github.com/repos/" +
+        username +
+        "/" +
+        repo +
+        "/contents" +
+        "/" +
+        structure,
+      method: "GET",
+      headers: {
+        "User-Agent": "request",
+        authorization: b_token,
+      },
+    };
 
-  // Return new promise
-  return new Promise(function (resolve, reject) {
-    // Do async job
-    request.get(options, function (err, resp, body) {
-      if (err) {
-        reject(err);
-      } else {
-        body = JSON.parse(body);
-        resolve(body);
-      }
+    // Return new promise
+    return new Promise(function (resolve, reject) {
+      // Do async job
+      request.get(options, function (err, resp, body) {
+        if (err) {
+          reject(err);
+        } else {
+          body = JSON.parse(body);
+          resolve(body);
+        }
+      });
     });
-  });
+  } else {
+    const options = {
+      url:
+        "https://api.github.com/repos/" +
+        username +
+        "/" +
+        repo +
+        "/contents" +
+        "/" +
+        structure,
+      method: "GET",
+      headers: {
+        "User-Agent": "request",
+      },
+    };
+
+    // Return new promise
+    return new Promise(function (resolve, reject) {
+      // Do async job
+      request.get(options, function (err, resp, body) {
+        if (err) {
+          reject(err);
+        } else {
+          body = JSON.parse(body);
+          resolve(body);
+        }
+      });
+    });
+  }
 }
 
 module.exports = getRepo;
