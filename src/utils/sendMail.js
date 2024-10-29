2947724cab0c75f967ad20e08499c40aecbc8184
Himanshu Aswal <aswalhim@amazon.com>
2024-10-29 06:42:19 +0530

    updating dependencies

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,12 +6,19 @@ const sendgridAPIKey = process.env.SENDGRID_API_KEY;
 
 sgMail.setApiKey(sendgridAPIKey);
 const sendMail = (email, subject, text) => {
-  sgMail.send({
-    to: email,
-    from: process.env.SENDERMAIL,
-    subject: subject,
-    text: text,
-  });
+  sgMail
+    .send({
+      to: email,
+      from: process.env.SENDERMAIL,
+      subject: subject,
+      text: text,
+    })
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 module.exports = sendMail;
